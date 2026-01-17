15f7081f2ad72d49b27c96dbd06da251d2dd9796
Sfabi28 <samuelefabi28@gmail.com>
2026-01-17 19:47:04 +0100

    app locked to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'screens/login_screen.dart'; // <--- Importa la LoginScreen
+import 'package:flutter/services.dart';
+import 'screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   try {
     await dotenv.load(fileName: ".env");
